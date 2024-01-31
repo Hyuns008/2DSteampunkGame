15a1e6cb9b2c8eccfc6d6745cb5950cb8e2e3877
Hyuns008 <gustn4984@naver.com>
2024-01-31 13:47:29 +0900

    글로벌 볼륨 테스트 스크립트 업로드

---
+++ Assets/Project/Scripts/Etc/TestVolume.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class TestVolume : MonoBehaviour
+{
+    [SerializeField] private Volume volume;
+
+    private void Awake()
+    {
+
+    }
+}
